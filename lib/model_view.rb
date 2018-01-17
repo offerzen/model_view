4ea43edcbd9870a141686ddfe7177ef249619ab0
Ethan Marrs <ethan@offerzen.com>
2018-01-17 15:36:00 +0200

    Fix bug overwriting parent scope
    * Use hash deep copy when referencing superclass scope

--- lib/model_view.rb
+++ lib/model_view.rb
@@ -102,7 +102,7 @@ module ModelView
 
   def create_scopes
     if self.superclass.respond_to?(:scopes) && @scopes.nil?
-      @scopes = self.superclass.scopes
+      @scopes = self.superclass.scopes.deep_dup
     elsif @scopes.nil?
       @scopes = { ROOT =>  new_opts }
     end
